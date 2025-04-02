658efbaf9f459956541e9139f13e05a8c55288ff
Sivakumar N <shivask94423@gmail.com>
2025-04-02 13:02:02 +0530

    updated moveing button logics.

--- script.js
+++ script.js
@@ -1,46 +1,79 @@
 let boxOne = document.getElementById('box-one');
-        let boxTwo = document.getElementById('box-two');
+let boxTwo = document.getElementById('box-two');
 
-        document.querySelector('.all-left')
-        .addEventListener('click', () => moveAll(boxTwo, boxOne));
+document.querySelector('.all-left')
+    .addEventListener('click', () => {
+        moveAll(boxTwo, boxOne);
+        updateButtons();
+    });
 
-        document.querySelector('.all-right')
-        .addEventListener('click', () => moveAll(boxOne, boxTwo));
+document.querySelector('.all-right')
+    .addEventListener('click', () => {
+        moveAll(boxOne, boxTwo);
+        updateButtons();
+    });
 
-        document.querySelector('.left')
-        .addEventListener('click', () => checkedMove(boxTwo, boxOne));
+document.querySelector('.left')
+    .addEventListener('click', () => {
+        checkedMove(boxTwo, boxOne);
+        updateButtons();
+    });
 
-        document.querySelector('.right')
-        .addEventListener('click', () => checkedMove(boxOne, boxTwo))
-        
-        function checkedMove (box1, box2) {
-            box1.querySelectorAll('input[type="checkbox"]:checked')
-            .forEach(input => {
-                input.checked =false;
-                box2.appendChild(input.parentNode);
-            });
-        }
+document.querySelector('.right')
+    .addEventListener('click', () => {
+        checkedMove(boxOne, boxTwo);
+        updateButtons();
+    })
 
-        function moveAll (box1, box2) {
-            box1.querySelectorAll('div').forEach(div => {
-                box2.appendChild(div);
-            });
-        }
-
-        boxOne.addEventListener('change', () => {
-            let clicked = boxOne.querySelectorAll('input[type=checkbox]:checked');
-            if(clicked.length > 0) {
-                document.querySelector('.left').setAttribute('disabled', false);
-            } else {
-                document.querySelector('.left').removeAttribute('disabled');
-            }
+function checkedMove(box1, box2) {
+    box1.querySelectorAll('input[type="checkbox"]:checked')
+        .forEach(input => {
+            input.checked = false;
+            box2.appendChild(input.parentNode);
         });
+}
 
-        boxTwo.addEventListener('change', () => {
-            let clicked = boxTwo.querySelectorAll('input[type=checkbox]:checked');
-            if(clicked.length > 0) {
-                document.querySelector('.right').setAttribute('disabled', false);
-            } else {
-                document.querySelector('.right').removeAttribute('disabled');
-            }
-        });
+function updateButtons() {
+    let hasBoxOne = boxOne.querySelector('div') !== null;
+    let hasBoxTwo = boxTwo.querySelector('div') !== null;
+
+    if (hasBoxOne) {
+        document.querySelector('.right').removeAttribute('disabled');
+        document.querySelector('.all-right').removeAttribute('disabled');
+    } else {
+        document.querySelector('.right').setAttribute('disabled', 'true');
+        document.querySelector('.all-right').setAttribute('disabled', 'true');
+    }
+
+    if (hasBoxTwo) {
+        document.querySelector('.left').removeAttribute('disabled');
+        document.querySelector('.all-left').removeAttribute('disabled');
+    } else {
+        document.querySelector('.left').setAttribute('disabled', 'true');
+        document.querySelector('.all-left').setAttribute('disabled', 'true');
+    }
+}
+
+function moveAll(box1, box2) {
+    box1.querySelectorAll('div').forEach(div => {
+        box2.appendChild(div);
+    });
+}
+
+boxOne.addEventListener('change', () => {
+    let clicked = boxOne.querySelectorAll('input[type=checkbox]:checked');
+    if (clicked.length > 0) {
+        document.querySelector('.left').setAttribute('disabled', false);
+    } else {
+        document.querySelector('.left').removeAttribute('disabled');
+    }
+});
+
+boxTwo.addEventListener('change', () => {
+    let clicked = boxTwo.querySelectorAll('input[type=checkbox]:checked');
+    if (clicked.length > 0) {
+        document.querySelector('.right').setAttribute('disabled', false);
+    } else {
+        document.querySelector('.right').removeAttribute('disabled');
+    }
+});
